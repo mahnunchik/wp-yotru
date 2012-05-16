8338b95c60ef235151f011f31e5bf12906f6ae78
Radist <Radist@169.254.199.208>
2012-05-16 20:03:26 +0600

    Settings description added

--- options.php
+++ options.php
@@ -8,8 +8,17 @@
 <table class="form-table">
 
 <tr valign="top">
-<th scope="row">Api key:</th>
-<td><input type="text" name="yotru_api_key" placeholder="<?php _e('Paste Yotru API key here', 'text_domain') ?>" value="<?php echo get_option('yotru_api_key'); ?>" /></td>
+    <td colspan="2">
+        <?php _e('Please, register on Yotru.com website and obtain API key. You could register <a href="http://yotru.com/#register">here</a>.', 'text_domain') ?>
+    </td>
+</tr>
+<tr valign="top">
+    <th scope="row">
+        Api key:
+    </th>
+    <td>
+        <input type="text" name="yotru_api_key" placeholder="<?php _e('Paste Yotru API key here', 'text_domain') ?>" value="<?php echo get_option('yotru_api_key'); ?>" />
+    </td>
 </tr>
 
 </table>
